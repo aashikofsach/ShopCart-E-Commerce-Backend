34b7191f782dacf38e400d5c9e9716049c932b54
aashish <ritikkamboj4314@gmail.com>
2026-03-15 22:47:32 +0530

    use and types of middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+const responseTime = require('response-time');
 
 const { PORT } = require("./config/server_config");
-const {router: apiRoutes} =  require("./routes/apiRoutes");
+const {router: apiRoutes} =  require("./routes/api_routes");
 const app = express();
+
+app.use(responseTime());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.use("/api", apiRoutes);
 
--- src/routes/v1/ping_routes.js
+++ src/routes/v1/ping_routes.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const { pingController } = require("../../controllers/ping_controller");
 
-const router = express.Router() ;
+const router = express.Router();
 
+function checkRequest(req, res, next) {
+  console.log("this is check middleware");
+  next();
+  console.log("ending checkRequest");
+}
 
-router.get("/", pingController);
+function logger(req, res, next) {
+  console.log("Inside the logger middleware");
+  next();
+  console.log("ending logger ");
+}
 
-module.exports = {router};
+function authChecker(req, res, next) {
+  console.log("this is authChecker middleware");
+  next();
+  console.log("ending the authChecker");
+}
+
+router.get("/", checkRequest, logger, authChecker, pingController);
+
+module.exports = { router };
