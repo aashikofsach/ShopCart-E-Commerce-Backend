2a9ac4064c60c57f6d45dc1540073b3fafec8f65
aashish <ritikkamboj4314@gmail.com>
2026-03-17 09:51:55 +0530

    added http response status

--- src/middlewares/product_middleware.js
+++ src/middlewares/product_middleware.js
@@ -1,10 +1,9 @@
+const {errorResponse} = require("../utils/error_response")
+
 function createProductvalidator(req, res, next) {
     console.log(req.body, "we are here")
   if (!req.body.title) {
-    return res.json({
-      success: false,
-      message: "Title is not present insider the req body",
-    });
+    return res.status(400).json(errorResponse("title is not present inside the req body", {message : "req body nopt have the title data"}));
   }
 
 
---
+++ src/utils/error_response.js
@@ -0,0 +1,14 @@
+function errorResponse(message , error)
+{
+    return {
+      success: false,
+      data : {},
+      message: message,
+      error : error
+    }
+
+}
+
+module.exports ={
+    errorResponse
+}
