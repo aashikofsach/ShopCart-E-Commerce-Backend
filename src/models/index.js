dc72806b74b2c88149ad0465c2016f9037aa30e2
aashish <ritikkamboj4314@gmail.com>
2026-03-28 22:52:54 +0530

    added user model

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 const Category = require("./categories");
 const Product = require("./products");
+const User = require("./user");
 
 Product.belongsTo(Category);
 Category.hasMany(Product, {
@@ -9,4 +10,5 @@ Category.hasMany(Product, {
 module.exports = {
   Product,
   Category,
+  User
 };
---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const Sequalize = require("sequelize");
+
+const db = require("../config/db_config");
+
+const User = db.define("user", {
+  email: {
+    type: Sequalize.STRING,
+    allowNull: false,
+    unique: true,
+    isEmail : true,
+  },
+  password: {
+    type: Sequalize.STRING,
+    allowNull: false,
+    len : [3,30],
+    isAlphanumeric : true 
+  },
+});
+
+module.exports = User;
