614c5e654771a0a02552741455779c747240df42
aashish <ritikkamboj4314@gmail.com>
2026-04-01 19:58:42 +0530

    created function to verify token send from client side

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,12 @@ function generateJWT(payload)
 {
     return jwt.sign(payload , JWT_SECRET , {expiresIn : JWT_EXPIRY})
 }
+function verifyToken(token)
+{
+    return jwt.verify(token , JWT_SECRET)
+}
 
 module.exports ={
-    generateJWT
+    generateJWT,
+    verifyToken
 }
