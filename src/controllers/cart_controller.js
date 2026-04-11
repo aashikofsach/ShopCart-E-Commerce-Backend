f9f66e3f0bcdf9263026867b7a32f9d16d12d041
aashish <ritikkamboj4314@gmail.com>
2026-04-11 19:23:28 +0530

    created cart controller update func

---
+++ src/controllers/cart_controller.js
@@ -0,0 +1,42 @@
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const {
+  createProductService,
+  getProductsServices,
+  getProductService,
+} = require("../services/product_service");
+
+// const ProductService = require("../services/product_service");
+// const CategoryService = require("../services/category_service");
+// const FakeStoreRepository = require("../repositories/fake_store_repository");
+// const CategoryRepository = require("../repositories/category_repository");
+const { errorResponse } = require("../utils/error_response");
+const { CartRepository } = require("../repositories");
+const { CartService } = require("../services");
+// const ProductRepository = require("../repositories/product_repository");
+
+const categoryService = new CartService(
+  new CartRepository()
+);
+
+async function updateCart(req, res) {
+  try {
+
+    const response = await CartService.updateCart(req.params.id , req.body.productId, shouldAddProduct);
+
+    return res.status(StatusCodes.OK).json({
+      sucess: true,
+      error: null,
+      message: "Updated Cart Successfully",
+      data: response,
+    });
+  } catch (error) {
+    console.log("Something went wrong Cart Controller", error);
+    return res
+      .status(error.statusCode)
+      .json(errorResponse(error.ReasonPhrases, error));
+  }
+}
+
+module.exports = {
+  updateCart
+};
