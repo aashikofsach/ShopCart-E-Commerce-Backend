5e90cb49f760b1770d62aa708a4210b3aabdc782
aashish <ritikkamboj4314@gmail.com>
2026-04-28 23:09:20 +0530

    test: enhance userService sign-in test with bcrypt mock implementation( after user is there mocking now checking password comparision mocking

--- tests/services/user.test.js
+++ tests/services/user.test.js
@@ -1,7 +1,9 @@
 const { UserRepository, CartRepository } = require("../../src/repositories/");
 const UserService = require("../../src/services/user_service");
+const bcrypt = require("bcryptjs");
 jest.mock("../../src/repositories/user_repository")
 jest.mock("../../src/repositories/cart_repository")
+
 
 const mockUser = {
   id: 1,
@@ -24,7 +26,11 @@ describe("Test for userService Sign in method", () => {
 
   test("should return valid jwt toke", async()=>{
     //prepare
-    const userService = new UserService(new UserRepository() , new CartRepository());
+    const userService = new UserService(new UserRepository() , new CartRepository()); 
+    // in signInUser method of userService, firstly we check if the user is present and then we compare the password 
+    //with bcrypt, and for that we have to make the mock of bcrypt
+    jest.spyOn("bcrypt", compare).mockImplementation(()=> true)
+
 
 
     //Act
