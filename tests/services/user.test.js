01f99d0fd653dc871cf2a5610e4578be1e047d55
aashish <ritikkamboj4314@gmail.com>
2026-05-02 17:29:36 +0530

    test: refactor userService sign-in test for improved structure and clarity completed

--- tests/services/user.test.js
+++ tests/services/user.test.js
@@ -1,8 +1,10 @@
 const { UserRepository, CartRepository } = require("../../src/repositories/");
 const UserService = require("../../src/services/user_service");
+const authUtils = require('../../src/utils/auth');
 const bcrypt = require("bcryptjs");
-jest.mock("../../src/repositories/user_repository")
-jest.mock("../../src/repositories/cart_repository")
+jest.mock("../../src/repositories/user_repository");
+jest.mock("../../src/repositories/cart_repository");
+jest.mock("../../src/utils/auth");
 
 
 const mockUser = {
@@ -20,21 +22,36 @@ describe("Test for userService Sign in method", () => {
         findUserByEmail: (email) => {
           return [mockUser];
         },
+        getUsers: () => {
+          return [mockUser];
+        },
+        getUser: () => {
+          return mockUser;
+        },
       };
     });
+
+    authUtils.generateJWT.mockImplementation(()=> 'token')
   });
 
-  test("should return valid jwt toke", async()=>{
+
+
+  test("should return valid jwt toke", async () => {
     //prepare
-    const userService = new UserService(new UserRepository() , new CartRepository()); 
-    // in signInUser method of userService, firstly we check if the user is present and then we compare the password 
+    const userService = new UserService(
+      new UserRepository(),
+      new CartRepository(),
+    );
+    // in signInUser method of userService, firstly we check if the user is present and then we compare the password
     //with bcrypt, and for that we have to make the mock of bcrypt( as it present in just next file call so we have to make mock of it )
-    jest.spyOn(bcrypt, "compare").mockImplementation(()=> true);
-
-
-
+    jest.spyOn(bcrypt, "compare").mockImplementation(() => true);
 
     //Act
-    const response = await userService.signInUser({email : "A@b.com", password :"12345"})
-  })
+    const response = await userService.signInUser({
+      email: "A@b.com",
+      password: "12345",
+    });
+
+    expect(response).toBe('token');
+  });
 });
